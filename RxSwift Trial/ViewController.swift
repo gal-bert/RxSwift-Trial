b25bc3617edec0d2a388f042dce2c007ebc729c5
Gregorius Albert <galbert.djaja@gmail.com>
2022-09-22 14:22:46 +0700

    Initial Commit

---
+++ RxSwift Trial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwift Trial
+//
+//  Created by Gregorius Albert on 22/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
